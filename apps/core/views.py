3f9f53616f50cdb0e93abe1c77a86b4637b1a54b
Esteban Madrigal <ememadrigal.em@gmail.com>
2021-04-26 23:55:03 -0600

    Main buttons added

--- apps/core/views.py
+++ apps/core/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def frontpage(request):
     return render(request,'frontpage.html')
+def contact(request):
+    return render(request,'contact.html')
