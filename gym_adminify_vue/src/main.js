80bae23be17cbce235d5b879884107cdf5323f88
Esteban Madrigal <ememadrigal.em@gmail.com>
2021-04-29 23:35:57 -0600

    v-ccalendar installed

--- gym_adminify_vue/src/main.js
+++ gym_adminify_vue/src/main.js
@@ -2,6 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VCalendar from 'v-calendar';
+
 
 createApp(App).use(store).use(router).mount('#app')
 
