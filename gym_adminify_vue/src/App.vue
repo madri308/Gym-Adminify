5b086ad053b6642f8086244eda2395dc64b10d38
Esteban Madrigal <ememadrigal.em@gmail.com>
2021-04-29 18:43:02 -0600

    Base template

--- gym_adminify_vue/src/App.vue
+++ gym_adminify_vue/src/App.vue
@@ -1,30 +1,71 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div id="wrapper">
+    <nav class="navbar is-dark">
+      <div class="navbar-brand">
+        <router-link to="/" class="navbar-item"><strong>Gym-Adminify</strong></router-link>
+
+        <a class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar-menu" @click="showMobileMenu = !showMobileMenu">
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+      <div class="navbar-menu" id="navbar-menu" v-bind:class="{'is-active': showMobileMenu }">
+        <div class="navbar-start">
+          <router-link to="/clientes" class="navbar-item">Clientes</router-link>
+          <router-link to="/servicios" class="navbar-item">Servicios</router-link>
+          <router-link to="/actividades" class="navbar-item">Actividades</router-link>
+          <router-link to="/facturas" class="navbar-item">Facturas</router-link>
+          <router-link to="/instructores" class="navbar-item">Instructores</router-link>
+          <!-- <div class="navbar-item">
+            <form method="get" action="/search">
+              <div class="field has-addons">
+                <div class="control">
+                  <input type="text" class="input" placeholder="What are you looking for?" name="query">
+                </div>
+
+                <div class="control">
+                  <button class="button is-success">
+                      <span class="icon">
+                      <i class="fas fa-search"></i>
+                      </span>
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div> -->
+        </div>
+      </div>
+      <div class="navbar-end">
+
+        <div class="navbar-item">
+          <div class="buttons">
+            <template v-if="$store.state.isAuthenticated">
+              <router-link to="/my-account" class="button is-light">My account</router-link>
+            </template>
+
+            <template v-else>
+              <router-link to="/log-in" class="button is-light">Log in</router-link>
+            </template>
+
+            <router-link to="/configuracion" class="button is-success">
+              <span class="icon"><i class="fas fa-cog"></i></span>
+            </router-link>
+            <router-link to="/log-in" class="button is-light">Salir</router-link>
+          </div>
+        </div>
+      </div>
+    </nav>
+    <section class="section">
+      <router-view/>
+    </section>
+    <footer class="footer">
+      <p class="has-text-centered">Copyright (c) 2021</p>
+    </footer>
+
   </div>
-  <router-view/>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+@import '../node_modules/bulma';
 </style>
