2b712dfecf830a20b71288102d09b5bea76ed9b9
Jose <mikomeka1@gmail.com>
2021-05-02 02:44:43 -0600

    Enabled close functionality Covid Message

--- gym_adminify_vue/src/App.vue
+++ gym_adminify_vue/src/App.vue
@@ -63,7 +63,7 @@
       </div>
     </nav>
 
-    <div class="bg-indigo-600">
+    <div  v-show='toggle' id="messageCovid" class="bg-indigo-600" >
       <div class="max-w-7xl mx-auto py-3 px-3 sm:px-6 lg:px-8">
         <div class="flex items-center justify-between flex-wrap">
           <div class="w-0 flex-1 flex items-center">
@@ -80,7 +80,7 @@
             </p>
           </div>
           <div class="order-2 flex-shrink-0 sm:order-3 sm:ml-3">
-            <button
+            <button @click='toggle = !toggle'
               type="button"
               class="-mr-1 flex p-2 rounded-md hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-white sm:-mr-2"
             >
@@ -117,8 +117,10 @@ export default {
   data() {
     return {
       showMobileMenu: false,
+      toggle: true,
     };
   },
+ 
   components: {
     SpeakerphoneIcon,
     XIcon,
