d010ced89052b7e899d98af1950994dceeec0832
Esteban Madrigal <ememadrigal.em@gmail.com>
2021-05-02 00:33:58 -0600

    Update Bills.vue
    Bills with for

--- gym_adminify_vue/src/views/Bills.vue
+++ gym_adminify_vue/src/views/Bills.vue
@@ -1,34 +1,29 @@
+<!-- This example requires Tailwind CSS v2.0+ -->
 <template>
-  <div class="w-full px-4 pt-16">
-    <div class="w-full max-w-md p-2 mx-auto bg-white rounded-2xl">
-      <Disclosure as="div" class="mt-2">
-        <DisclosureButton
-          class="flex justify-between w-full px-4 py-2 text-sm font-medium text-left text-purple-900 bg-purple-100 rounded-lg hover:bg-purple-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75"
+  <div class="bg-white">
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div class>
+        <dl
+          class="space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10"
         >
-          <span>Do you offer technical support?</span>
-          <ChevronUpIcon
-            :class="open ? 'transform rotate-180' : ''"
-            class="w-5 h-5 text-purple-500"
-          />
-        </DisclosureButton>
-        <DisclosurePanel class="px-4 pt-4 pb-2 text-sm text-gray-500">
-          <Disclosure>
-        <DisclosureButton
-          class="flex justify-between w-full px-4 py-2 text-sm font-medium text-left text-purple-900 bg-purple-100 rounded-lg hover:bg-purple-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75"
-        >
-          <span>What is your refund policy?</span>
-          <ChevronUpIcon
-            :class="open ? 'transform rotate-180' : ''"
-            class="w-5 h-5 text-purple-500"
-          />
-        </DisclosureButton>
-        <DisclosurePanel class="px-4 pt-4 pb-2 text-sm text-gray-500">
-          If you're unhappy with your purchase for any reason, email us within
-          90 days and we'll refund you in full, no questions asked.
-        </DisclosurePanel>
-      </Disclosure>
-        </DisclosurePanel>
-      </Disclosure>
+          <div v-for="feature in features" :key="feature.name" class="relative">
+            <Disclosure as="div" class="mt-2">
+              <DisclosureButton
+                class="flex justify-between w-full px-4 py-2 text-sm font-medium text-left text-purple-900 bg-purple-100 rounded-lg hover:bg-purple-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75"
+              >
+                <span>{{feature.name}}</span>
+                <ChevronUpIcon
+                  :class="open ? 'transform rotate-180' : ''"
+                  class="w-5 h-5 text-purple-500"
+                />
+              </DisclosureButton>
+              <DisclosurePanel class="px-4 pt-4 pb-2 text-sm text-gray-500">
+                {{feature.description}}
+              </DisclosurePanel>
+            </Disclosure>
+          </div>
+        </dl>
+      </div>
     </div>
   </div>
 </template>
@@ -36,9 +31,47 @@
 <script>
 import { Disclosure, DisclosureButton, DisclosurePanel } from "@headlessui/vue";
 import { ChevronUpIcon } from "@heroicons/vue/solid";
+import {
+  AnnotationIcon,
+  GlobeAltIcon,
+  LightningBoltIcon,
+  ScaleIcon,
+} from "@heroicons/vue/outline";
+
+const features = [
+  {
+    name: "Competitive exchange rates",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
+    icon: GlobeAltIcon,
+  },
+  {
+    name: "No hidden fees",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
+    icon: ScaleIcon,
+  },
+  {
+    name: "Transfers are instant",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
+    icon: LightningBoltIcon,
+  },
+  {
+    name: "Mobile notifications",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
+    icon: AnnotationIcon,
+  },
+];
 
 export default {
   name: "Bill",
   components: { Disclosure, DisclosureButton, DisclosurePanel, ChevronUpIcon },
+  setup() {
+    return {
+      features,
+    };
+  },
 };
 </script>
