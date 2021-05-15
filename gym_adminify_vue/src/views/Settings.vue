b4dbb4e37549304655426f3598fe2d2f11be1357
Esteban Madrigal <ememadrigal.em@gmail.com>
2021-05-15 13:33:46 -0600

    Gym settings done
    Config, gym and room can be edited

--- gym_adminify_vue/src/views/Settings.vue
+++ gym_adminify_vue/src/views/Settings.vue
@@ -1,17 +1,31 @@
 <template>
   <div class="page-settings">
+    <div v-if="canChangeSettings">
+      <button v-if="!isChanging" v-on:click ='isChanging = !isChanging' class="fixed z-50 bottom-10 right-10 w-12 h-12 bg-blue-500 rounded-full hover:bg-blue-700 active:shadow-lg mouse shadow transition ease-in duration-200 focus:outline-none">
+        <i class="text-white fas fa-pencil-alt fa-lg"></i>
+      </button>
+      <button v-else v-on:click ='changeSettings()' class="fixed z-50 bottom-10 right-10 w-12 h-12 bg-green-500 rounded-full hover:bg-green-700 active:shadow-lg mouse shadow transition ease-in duration-200 focus:outline-none">
+        <i class="text-white far fa-save fa-lg"></i>
+      </button>
+    </div>
     <div class="columns is-multiline">
 
       <div class="column is-5">
         <section class="flex flex-wrap justify-center px-4 bg-white">
           <div class="max-w-lg w-full rounded-lg shadow-lg p-4">
 
-            <h1 class="title">{{ gym.name }}</h1>
-            <p>Costo de matricula: ₡{{ gym.tuitioncost }}</p>
+            <input :disabled="!isChanging" v-model="gym.name" class="title appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder="Fecha inicio" aria-label="Full name">
+
+            <span class="font-extrabold">Costo de matricula:</span>
+            <input :disabled="!isChanging" v-model="gym.tuitioncost" class="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder="Fecha inicio" aria-label="Full name">
 
             <h1 class="title mt-4">Configuracion General</h1>
-            <p>Fecha inicio: {{ config.effectivedate }}</p>
-            <p>Porcentaje de capacidad: {{ config.capacitypercentage }}</p>
+
+            <span class="font-extrabold">Fecha inicio:</span>
+            <input :disabled="!isChanging" v-model="config.effectivedate" class="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder="Fecha inicio" aria-label="Full name">
+
+            <span class="font-extrabold">Porcentaje de capacidad:</span> 
+            <input :disabled="!isChanging" v-model="config.capacitypercentage" class="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder="Fecha inicio" aria-label="Full name">
 
             <h1 class="title mt-4">Horario General</h1>
             <div class="table-responsive">
@@ -25,26 +39,25 @@
                 </thead>
                 <tbody>
                   <tr v-for="item in config.timeperday" :key="item">
-                    <td>{{ item.dia }}</td>
-                    <td>{{ item.inicio }}</td>
-                    <td>{{ item.fin }}</td>
+                    <td><input :disabled="true" v-model="item.dia" type="day" ></td>
+                    <td><input :disabled="!isChanging" v-model="item.inicio" type="time" ></td>
+                    <td><input :disabled="!isChanging" v-model="item.fin" type="time" ></td>
+                    <button v-if="isChanging" v-on:click ='deleteDay(item)' type="button" class=" rounded-md transition hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-white sm:-mr-2">
+                      <i class="far fa-times-circle fa-sm"></i>
+                    </button>
                   </tr>
                 </tbody>
               </table>
             </div>
-
           </div>
         </section>
       </div>
-
       <div class="column is-5">
         <section class="flex flex-wrap px-4 bg-white">
-
           <div class="max-w-lg w-full rounded-lg shadow-lg p-4">
             <h1 class="title">Room-1</h1>
             <p>Info del room</p>
           </div>
-          
         </section>
       </div>
     </div>
@@ -62,73 +75,70 @@ export default {
       gym: {},
       room: {},
       config: {},
+      isChanging: false,
+      permissions: this.$store.state.permissions,
     };
   },
   mounted() {
-    this.getConfig();
-    this.getGym();
-    this.getRoom();
+    this.getSettings();
+  },
+  computed:{
+    canChangeSettings(){
+      return this.permissions.includes("gymSettings.change_gym") 
+          && this.permissions.includes("gymSettings.change_room") 
+          && this.permissions.includes("gymSettings.change_config")
+    },
   },
   methods: {
-    async getConfig() {
-      this.$store.commit("setIsLoading", true);
-      await axios
-        .get("/api/v1/gym-config/")
-        .then((response) => {
-          this.config = response.data;
-        })
-        .catch((error) => {
-          // console.log(error);
-          toast({
-            message: "Ocurrio un problema con los datos de: Configuracion",
-            type: "is-danger",
-            dismissible: true,
-            pauseOnHover: true,
-            duration: 2000,
-            position: "bottom-right",
-          });
-        });
-      this.$store.commit("setIsLoading", false);
+    deleteDay(item){
+      item.fin = null,
+      item.inicio = null
     },
-    async getGym() {
+    async getSettings(){
       this.$store.commit("setIsLoading", true);
       await axios
-        .get("/api/v1/gym-info/")
+        .get("/api/v1/update-settings/")
         .then((response) => {
-          this.gym = response.data;
+          this.config = response.data['config'];
+          this.gym = response.data['gym'];
+          this.room = response.data['room'];
           document.title = this.gym.name;
         })
         .catch((error) => {
-          // console.log(error.response);
           toast({
-            message: "Ocurrio un problema con los datos de: Gimnasio", type: "is-danger",
+            message: "Ocurrio un problema obteniendo las configuraciones", type: "is-danger",
             dismissible: true, pauseOnHover: true,
             duration: 2000, position: "bottom-right",
           });
-          this.$router.push({ name: 'Home' })
         });
       this.$store.commit("setIsLoading", false);
     },
-    async getRoom() {
+    async changeSettings(){
       this.$store.commit("setIsLoading", true);
+      const formData = {
+        config:this.config,
+        room: this.room,
+        gym: this.gym,
+      }
       await axios
-        .get("/api/v1/room-info/")
+        .put("/api/v1/update-settings/",formData)
         .then((response) => {
-          this.room = response.data;
+          toast({
+            message: "Configuracion editada exitosamente", type: "is-success",
+            dismissible: true, pauseOnHover: true,
+            duration: 3000, position: "bottom-right",
+          });
+          this.isChanging = !this.isChanging
         })
         .catch((error) => {
           toast({
-            message: "Ocurrio un problema con los datos de: Cuartos",
-            type: "is-danger",
-            dismissible: true,
-            pauseOnHover: true,
-            duration: 2000,
-            position: "bottom-right",
+            message: "Ocurrio un problema al editar la configuracion", type: "is-danger",
+            dismissible: true, pauseOnHover: true,
+            duration: 3000, position: "bottom-right",
           });
         });
       this.$store.commit("setIsLoading", false);
     },
-    
   },
 };
 </script>
