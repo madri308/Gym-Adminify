34db0f779fa599c1ea3dc834ec31fdfab4084fff
Jose <mikomeka1@gmail.com>
2021-05-15 15:17:03 -0600

    Modified Loading SVG
    Added a better looking DNA-like SVG

--- gym_adminify_vue/src/App.vue
+++ gym_adminify_vue/src/App.vue
@@ -82,7 +82,9 @@
     </div>
 
     <div class="is-loading-bar has-text-centered" v-bind:class="{ 'is-loading': $store.state.isLoading }">
-      <div class="lds-dual-ring"></div>
+      <div class="flex flex-wrap content-center">
+        <img class="transform scale-125 md:scale-125 sm:scale-75" src="http://vuejscomponent.com/_nuxt/img/loading.b3814b2.svg"/>
+      </div>
     </div>
 
     <section class="section">
@@ -144,37 +146,20 @@ export default {
 
 <style lang="scss">
 @import "../node_modules/bulma";
-.lds-dual-ring {
-  display: inline-block;
-  width: 80px;
-  height: 80px;
-}
-.lds-dual-ring:after {
-  content: " ";
-  display: block;
-  width: 64px;
-  height: 64px;
-  margin: 8px;
-  border-radius: 50%;
-  border: 6px solid #ccc;
-  border-color: #ccc transparent #ccc transparent;
-  animation: lds-dual-ring 1.2s linear infinite;
-}
-@keyframes lds-dual-ring {
-  0% {
-    transform: rotate(0deg);
-  }
-  100% {
-    transform: rotate(360deg);
-  }
-}
+
+
 .is-loading-bar {
   height: 0;
   overflow: hidden;
-  -webkit-transition: all 0.3s;
-  transition: all 0.3s;
+  -webkit-transition: all 0.6s;
+  transition: all 0.6s;
+  position: absolute;
+  top: 18%;
+  left: 50%;
+  transform: translate(-50%, -50%);
   &.is-loading {
     height: 80px;
+
   }
 }
 </style>
