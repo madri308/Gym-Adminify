be731c0002a083cf884641ba8aeebfd222be4f94
Jose <mikomeka1@gmail.com>
2021-05-15 13:54:59 -0600

    Auto stash before merge of "Jose" and "origin/Jose"

--- gym_adminfy_django/gymServices/urls.py
+++ gym_adminfy_django/gymServices/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 from gymServices import views
 
 urlpatterns = [
-    path('services/', views.AllServices.as_view())
+    path('services/', views.AllServices.as_view()),
+    path('services/<int:service_id>/', views.ServiceDetail.as_view()),
 ]
