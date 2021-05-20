1342123356389b4e9a33d4cd73a2ff9ed05f1673
Esteban Madrigal <ememadrigal.em@gmail.com>
2021-05-19 18:03:52 -0600

    Activity detail added

--- gym_adminfy_django/gymActivities/urls.py
+++ gym_adminfy_django/gymActivities/urls.py
@@ -5,6 +5,5 @@ from gymActivities import views
 urlpatterns = [
     path('activities/', views.AllActivities.as_view()),
     path('schedule-activities/', views.AllScheduleActivities.as_view()),
-    #path('activities/', views.AllActivities.as_view()),
-    #path('clientsPerActivity/<int:activity_id>', views.AllClientsByActivity.as_view()),
+    path('activities/<int:activity_id>/', views.ActivityDetail.as_view()),
 ]
