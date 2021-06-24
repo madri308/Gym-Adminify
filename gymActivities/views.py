48d7b659736b1efecc329dc249195b95e1690621
Mariell Sanchez <mariellsanchez@estudiantec.cr>
2021-06-24 12:04:41 -0600

    Can edit activity details

--- gymActivities/views.py
+++ gymActivities/views.py
@@ -174,7 +174,8 @@ class ActivityEnrollClients(ListCreateAPIView):
             for element in clients_enroll:
                 client = Client.objects.get(person_id=element)
                 if (client.clientstate.name != "Activo"):
-                    return Response(status=status.HTTP_406_NOT_ACCEPTABLE)
+                    content = {'Error': 'Hay un cliente moroso'}
+                    return Response(content,status=status.HTTP_406_NOT_ACCEPTABLE)
                 act.client.add(client)
                 b = Bill(
                     paid = 0,
@@ -212,17 +213,35 @@ class ActivityDetail(RetrieveUpdateDestroyAPIView):
         return ActivitiesSerializer
 
     services = Service.objects.all()
-    config = Config.objects.last()
     def get(self, request, *args, **kwargs):
         service_ser = ServiceSerializer(self.services,many=True)
-        config_ser = ConfigSerializer(self.config, many=False)
-        return Response({'service':service_ser.data, "config":config_ser.data})
+        return Response({'service':service_ser.data})
 
     def delete(self, request, activity_id, format=None):
         activity = Activity.objects.get(pk=activity_id)
         activity.delete()
         
         return Response(status=status.HTTP_200_OK)
+
+    def put(self, request, *args, **kwargs):
+        activity = Activity.objects.get(id=kwargs['activity_id'])
+        activities_related = Activity.objects.all().filter(dayofweek = activity.dayofweek, 
+                                                            startime = activity.startime) 
+        for act in activities_related:
+            act.capacity = request.data['capacity']
+            act.dayofweek = request.data['dayofweek']
+            act.startime = request.data['startime']
+            act.endtime = request.data['endtime']
+            act.save(update_fields=['capacity','dayofweek', 'startime','endtime'])
+        return Response(status=status.HTTP_202_ACCEPTED)
+
+
+class ActivityTeacher(RetrieveUpdateDestroyAPIView):
+    model = Activity
+    def get_serializer_class(self):
+        if self.request.method == 'PUT':
+            return ActivitiesSerializer
+        return ActivitiesSerializer
 
     def put(self, request, *args, **kwargs):
         activity = Activity.objects.get(id=kwargs['activity_id'])
